d7bfc4a6a3c3ccb8421b646f193674ac6f6144ec
Sashank <sashankmeka7@gmail.com>
2021-02-16 11:03:14 -0800

    wrong file ooops

--- guessing.cpp
+++ guessing.cpp
@@ -1,28 +1,33 @@
 #include <stdlib.h>
 #include<iostream>
 using namespace std;
-bool game(int num){
+bool game(int num, int guesses){
 	int guess;//guessing function get the guess
 	cout << "gib guess"<<endl;
 	cin >> guess;//check guess recurse if wrong
 	if(guess>num){
+		guesses++;
 		cout <<"you overshot"<<endl;
-		return	game(num);
+		return	game(num, guesses);
 	}
 	if(guess < num){
+		guesses++;
 		cout << "you undershot"<<endl;
-		return game(num);
+		return game(num, guesses);
 	}
 	if(guess == num){
+		guesses++;
+		cout << "Guesses "<< guesses<<endl;
 		return true;
 	}
 	return false;
 }
 int main(){
+	int guesses = 0;
 	srand(time(NULL));//set time
 	int num = rand()%101;
 	char response[3];
-	if(game(num)){//ask to play again and exit or play again
+	if(game(num, guesses)){//ask to play again and exit or play again
 		cout << "YOU GOT IT! type yes or no if you want to either play again or quit"<< endl;
 		cin >> response;
 		if(response[0]=='y'){
